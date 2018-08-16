3ecdff4d015ba63bf50c934b9e67e3b56e9f8cc7
Steve <w0263155@nscc.ca>
2018-08-16 11:22:21 -0300

    Plays sound effect when button pressed

--- app/src/main/java/com/example/fuckinaroundapp/MainActivity.java
+++ app/src/main/java/com/example/fuckinaroundapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fuckinaroundapp;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         MediaPlayer song = MediaPlayer.create(MainActivity.this,R.raw.bit_song);
         song.start();
         song.setLooping(true);
+
+        final MediaPlayer soundEffect = MediaPlayer.create(MainActivity.this,R.raw.stupid);
+        Button playStupid = this.findViewById(R.id.button3);
+        playStupid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundEffect.start();
+            }
+        });
     }
 }
